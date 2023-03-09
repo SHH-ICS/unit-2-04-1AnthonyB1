04cd9b8b9e8fed8591b16bdb1ec553e17a6dac08
1AnthonyB1 <124379293+1AnthonyB1@users.noreply.github.com>
2023-03-09 03:25:00 +0000

    FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,3 @@
-# My Python Program
-# Task:  use if statements to output the result of the game fizzbuzz.  
-# Start at 1
-# For multiples of 3, output   Fizz
-# For multiples of 5, output   Buzz
-# For multiples of 15, output   FizzBuzz
-# End at 32
-
 for number in range(1,32):
     if((number % 3 == 0) & (number % 15 != 0)):
       print("Fizz")
